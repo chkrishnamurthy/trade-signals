74e333107f11d146ca785a36559b19d1b8db41e9
chkrishnamurthy <chalapatikrishna@gmail.com>
2026-09-14 22:36:28 +0530

    minor fix

--- packages/db/drizzle/0019_trim_low_value_audit_logs.sql
+++ packages/db/drizzle/0019_trim_low_value_audit_logs.sql
@@ -4,19 +4,13 @@
 -- removes low-value routine events and scrubs old browser strings that were
 -- previously stored in user_agent.
 
-DO $$
-BEGIN
-  EXECUTE 'ALTER TABLE auth_audit DISABLE TRIGGER auth_audit_no_mutation';
-
-  UPDATE auth_audit
-  SET user_agent = NULL
-  WHERE user_agent IS NOT NULL;
-
-  DELETE FROM auth_audit
-  WHERE event IN ('logout', 'email_verified', 'verification_resent');
-
-  EXECUTE 'ALTER TABLE auth_audit ENABLE TRIGGER auth_audit_no_mutation';
-EXCEPTION WHEN OTHERS THEN
-  EXECUTE 'ALTER TABLE auth_audit ENABLE TRIGGER auth_audit_no_mutation';
-  RAISE;
-END $$;
+ALTER TABLE auth_audit DISABLE TRIGGER auth_audit_no_mutation;
+--> statement-breakpoint
+UPDATE auth_audit
+SET user_agent = NULL
+WHERE user_agent IS NOT NULL;
+--> statement-breakpoint
+DELETE FROM auth_audit
+WHERE event IN ('logout', 'email_verified', 'verification_resent');
+--> statement-breakpoint
+ALTER TABLE auth_audit ENABLE TRIGGER auth_audit_no_mutation;
